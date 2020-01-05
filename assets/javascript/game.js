cf36ab745148679d7672547a4d2017ad44b2d5bd
josemm83 <josemmancha@gmail.com>
2020-01-04 23:30:59 -0600

    updating homework

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,61 @@
 //hp = health points, ap = attack power, cap = counter attack power
-var obiWan = {
-    HP: 120,
-    AP: 6,
-    CAP: 8
+var obiwan = {
+    hp: 120,
+    ap: 6,
+    cap: 8
 };
 
 var luke = {
-    HP: 100,
-    AP: 5,
-    CAP: 6
+    hp: 100,
+    ap: 5,
+    cap: 6
 };
 
 var sidious = {
-    HP: 150,
-    AP: 8,
-    CAP: 7
+    hp: 150,
+    ap: 8,
+    cap: 7
 };
 
 var grievous = {
-    HP: 100,
-    AP: 7,
-    CAP: 5
+    hp: 100,
+    ap: 7,
+    cap: 5
 };
 
-function attack(){
-    $("#attack").on("click"), function(){
+var defenseSelect = false;
+var offenseSelect = false;
+var attacker;
+var defenders=[];
 
+function attack(warrior){
+    $("#attack").click(function(){
+        // console.log(warrior);
+        warrior.ap += 6;
+        // console.log("attacker's new attack power: " + warrior.ap);
+    });
+}
+
+function defenderChoice(){
+    if(offenseSelect){
+        $("#luke").on("click", function(){
+            $("#luke").appendTo("#defender");    
+        });
+        defender = true;
+    }
+
+    else if(defender) {
+
+    }
+
+    else {
+        alert("Please select a defender");
     }
 }
 
-function defense(){
-
+function gameStart(offense){
+    defenderChoice();
+    attack(offense);
 }
 
 $(document).ready(function(){
@@ -40,17 +65,37 @@ $(document).ready(function(){
         $("#luke").appendTo("#defense");
         $("#sidious").appendTo("#defense");
         $("#grievous").appendTo("#defense");
+        offenseSelect = true;
+        gameStart(obiwan);
     });
 
     $("#luke").on("click", function (){
         alert("I am a Jedi like my father");
+        $("#obiwan").appendTo("#defense");
+        $("#luke").appendTo("#selection");
+        $("#sidious").appendTo("#defense");
+        $("#grievous").appendTo("#defense");
+        offenseSelect = true;
+        attacker = luke;
     });
 
     $("#sidious").on("click", function(){
         alert("I am the Senate!");
+        $("#obiwan").appendTo("#defense");
+        $("#luke").appendTo("#defense");
+        $("#sidious").appendTo("#selection");
+        $("#grievous").appendTo("#defense");
+        offenseSelect = true;
+        attacker = sidious;
     });
 
     $("#grievous").on("click", function(){
         alert("Prepare for attack!");
+        $("#obiwan").appendTo("#defense");
+        $("#luke").appendTo("#defense");
+        $("#sidious").appendTo("#defense");
+        $("#grievous").appendTo("#selection");
+        offenseSelect = true;
+        attacker = grievous;
     });
 });
